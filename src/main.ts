8de603bcf3cdf68959c82b4b136f7ea75b5147f9
Fartan <davilopescosta@outlook.com>
2019-11-17 01:28:33 -0300

    Config dotenv added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  app.setGlobalPrefix(process.env.API_PREFIX);
+
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
